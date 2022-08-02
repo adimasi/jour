59f7564cb8169eb0316a7bd285959b70fc5e4d66
Alessandro Dimasi <ale.dimasi47@gmail.com>
2022-08-03 00:01:49 +0200

    Fix bug on darkmode

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,8 +11,8 @@ import { todos } from "$lib/stores/todos.js";
 	<Todo id={todo.id} content={todo.content} />
 {:else}
 <div class="py-8 w-full flex flex-col items-center font-light">
-	<h3 class="text-3xl text-neutral-700 dark text-neutral-500">Nulla! Rilassati</h3>
-	<p class="text-sm text-neutral-700 dark text-neutral-500">(o aggiungi qualcosa da fare)</p>
+	<h3 class="text-3xl text-neutral-700 dark:text-neutral-500">Nulla! Rilassati</h3>
+	<p class="text-sm text-neutral-700 dark:text-neutral-500">(o aggiungi qualcosa da fare)</p>
 </div>
 {/each}
 </div>
