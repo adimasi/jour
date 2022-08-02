165941bf11beacac5ed1e46a1a6c87885150cdd6
Alessandro Dimasi <ale.dimasi47@gmail.com>
2022-08-03 00:00:51 +0200

    Remove greeting and add no todos phrase

--- src/lib/TodoInput.svelte
+++ src/lib/TodoInput.svelte
@@ -22,7 +22,7 @@ const addTodo = () => {
 
 <form class="text-xl flex gap-2" on:submit|preventDefault={addTodo}>
 	<input type="text" bind:value={todoContent} placeholder="Devo fare..."
-	class="grow px-8 py-4 rounded-md border-2 bg-transparent outline-none placeholder:font-light placeholder:text-lg placeholder:text-neutral-600 placeholder:dark:text-neutral-300 border-neutral-600 dark:border-neutral-600 focus:border-neutral-900 focus:dark:border-neutral-400">
+	class="grow px-8 py-4 rounded-md border-2 bg-transparent outline-none placeholder:font-light placeholder:text-lg placeholder:text-neutral-600 placeholder:dark:text-neutral-500 border-neutral-600 dark:border-neutral-600 focus:border-neutral-900 focus:dark:border-neutral-400">
 	<button class="flex-initial px-8 py-4 bg-neutral-800 text-neutral-100 dark:bg-neutral-200 dark:text-neutral-900 rounded-md"
 	type="submit">&plus;</button>
 </form>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,11 +5,14 @@ import Todo from "$lib/Todo.svelte";
 import { todos } from "$lib/stores/todos.js";
 </script>
 
-<Header />
 <TodoInput />
-
 <div>
 {#each $todos as todo (todo.id)}
 	<Todo id={todo.id} content={todo.content} />
+{:else}
+<div class="py-8 w-full flex flex-col items-center font-light">
+	<h3 class="text-3xl text-neutral-700 dark text-neutral-500">Nulla! Rilassati</h3>
+	<p class="text-sm text-neutral-700 dark text-neutral-500">(o aggiungi qualcosa da fare)</p>
+</div>
 {/each}
 </div>
